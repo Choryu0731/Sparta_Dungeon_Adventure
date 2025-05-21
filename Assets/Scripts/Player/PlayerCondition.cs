0e1f518697086e85acf7143c4dc5375b8a7f51ca
Choryu0731 <nabiiydg@naver.com>
2025-05-21 17:07:41 +0900

    feat. 플레이어 이동, 컨디션, 체력 UI 구현
    feat. 플레이어 이동, 컨디션, 체력 UI 구현

---
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamagable
+{
+    void TakePhysicalDamage(int damage);
+}
+
+public class PlayerCondition : MonoBehaviour, IDamagable
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition stamina { get { return uiCondition.stamina; } }
+
+    public event Action onTakeDamage;
+
+    void Update()
+    {
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if (health.curValue == 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+    
+    public void Die()
+    {
+        Debug.Log("ав╬З╢ы!");
+    }
+
+    public void TakePhysicalDamage(int damage)
+    {
+        health.Subtract(damage);
+        onTakeDamage?.Invoke();
+    }
+
+    public bool UseStamina(float amount)
+    {
+        if (stamina.curValue - amount < 0f)
+        {
+            return false;
+        }
+
+        stamina.Subtract(amount);
+        return true;
+    }
+}
